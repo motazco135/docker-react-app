616f83e7fdf76a8215354c77f91156aa4d50371e
Motaz Mohammed <motazco135@gmail.com>
2020-10-07 21:15:44 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Motaz <code>src/App.js</code> and save to reload.
+          Hi Motaz running from AWS docker <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
